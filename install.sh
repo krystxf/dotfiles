0078491b1077c3c5e813a59e3a32a71d3eecaf21
Krystof <krystof.kratky2003@gmail.com>
2024-05-28 18:19:14 -0600

    feat: update
    idk what more should I say, removed stuff I don't use anymore, added some gitconfig

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-cp .vimrc ~
-cp .tmux.conf ~
-cp .aliases ~
-cp .config/i3/config ~/.config/i3/config
-cp .config/terminator/config ~/.config/terminator/config
+cp .vimrc ~ 
+cp .aliases ~ 
+cat .zshrc >> ~/.zshrc
